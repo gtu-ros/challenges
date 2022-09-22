e288c8d9f6aa4cfeceb32ab8dfd9ff5316b07095
caglayurdasal <caglayurdasal@gmail.com>
2022-09-22 12:16:05 +0300

    Create method for updating velocity

--- scripts/draw_triangle.py
+++ scripts/draw_triangle.py
@@ -1,9 +1,69 @@
 #!/usr/bin/env python3
 import rospy
-from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 from challenge1.msg import Progress
 
+PI = 3.14
 
-def draw():
-    rospy.init_node("draw_triangle", anonymous=True)
-    pub = rospy.Publisher("draw_percent", Progress, queue_size=10)
+
+class MyTurtle:
+    def __init__(self):
+        rospy.init_node("draw_triangle", anonymous=True)
+
+        self.velocity_pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=10)
+        self.vel = Twist()
+        self.rate = rospy.Rate(10)
+
+    def update_vel(self, is_angular, is_linear, velocity, distance):
+        t0 = rospy.Time.now().to_sec()
+        if is_linear == True:
+            self.vel.linear.x = 1.0
+            self.vel.linear.y = 0.0
+            self.vel.linear.z = 0.0
+            self.vel.angular.x = 0.0
+            self.vel.angular.y = 0.0
+            self.vel.angular.z = 0.0
+
+            distance_taken = 0
+
+            while distance > distance_taken:
+                self.velocity_pub.publish(self.vel)
+                t1 = rospy.Time.now().to_sec()
+                distance_taken = velocity * (t1 - t0)
+        else:
+            current_angle = 0.0
+            angular_speed = 20 * 2 * PI / 360
+            relative_angle = 120 * 2 * PI / 360  # Equaliteral triangle
+            self.vel.linear.x = 0.0
+            self.vel.linear.y = 0.0
+            self.vel.linear.z = 0.0
+            self.vel.angular.x = 0.0
+            self.vel.angular.y = 0.0
+            self.vel.angular.z = angular_speed
+
+            while relative_angle > current_angle:
+                self.velocity_pub.publish(self.vel)
+                t1 = rospy.Time.now().to_sec()
+                current_angle = angular_speed * (t1 - t0)
+
+    def draw(self):
+        while not rospy.is_shutdown():
+            side_length = float(input("Enter side length: "))
+            if side_length > 0.0:
+                self.update_vel(False, True, 1, side_length)
+                self.update_vel(True, False, 0.5, 0)
+                self.update_vel(False, True, 1, side_length)
+                self.update_vel(True, False, 0.5, 0)
+                self.update_vel(False, True, 1, side_length)
+                break
+
+            else:
+                break
+
+
+if __name__ == "__main__":
+    try:
+        turtle = MyTurtle()
+        turtle.draw()
+    except rospy.ROSInterruptException:
+        pass
