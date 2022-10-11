2279ce89559a5cf8eff4a92a1371820ccd21dcba
ikbalarg <ikbalarguc2404@gmail.com>
2022-10-11 19:48:58 +0300

    rosparam set eklendi

--- scripts/draw_triangle.py
+++ scripts/draw_triangle.py
@@ -1,26 +1,21 @@
-from tabnanny import check
+
 import rospy
 from geometry_msgs.msg  import Twist
 from turtlesim.msg import Pose
-from math import pow,atan2,sqrt
+#from challenges.msg import Completed
 PI = 3.1415926535897
 
 class turtlebot():
 
     def __init__(self):
         #Creating our node,publisher and subscriber
-        rospy.init_node('turtlebot_controller', anonymous=True)
+        rospy.init_node('draw_triangle', anonymous=True)
         self.velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-        self.pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, self.callback)
+        #self.progress_publisher = rospy.Publisher("/challenge/draw_percent", Complated, queue_size=10)
+        self.pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose)
         self.pose = Pose()
         self.rate = rospy.Rate(10)
-        self.speed=1
-
-    #Callback function implementing the pose value received
-    def callback(self, data):
-        self.pose = data
-        self.pose.x = round(self.pose.x, 4)
-        self.pose.y = round(self.pose.y, 4)
+        self.speed=rospy.get_param("turtle_speed")
 
     def move_in_line(self,side_length):
         vel_msg = Twist()
@@ -44,7 +39,7 @@ class turtlebot():
     def rotate(self):
         vel_msg=Twist()
         vel_msg.angular.z = self.speed
-        t0	= rospy.Time.now().to_sec()
+        t0= rospy.Time.now().to_sec()
         angle_travelled = 0
 
         while ( angle_travelled < PI*(2/3) ):
@@ -54,18 +49,18 @@ class turtlebot():
 
         vel_msg.angular.z = 0
         self.velocity_publisher.publish(vel_msg)
-    def final(self):
-        a=float(input("Give a number:"))
+    def move_turtle(self):
+        a=float(input("Length:"))
         x = turtlebot()
         for i in range(3):
             x.move_in_line(a)
             x.rotate()
         
-        kontrol=str(input("close ---> 'q' ,'Q' or shutdown to rospy \n continue ---> Press any key except 'q'\n Press key:"))
-        a=kontrol=="q" or kontrol=="Q"
-        if a==False:
+        kontrol=str(input('if you want to draw a new triangle, press the "Y" key. If you want to exit, press any key other than "Y".'))
+        a=kontrol=="y" or kontrol=="Y"
+        if a==True:
             x = turtlebot()
-            x.final()
+            x.move_turtle()
         else:
             quit()
 
@@ -73,7 +68,8 @@ if __name__ == '__main__':
     try:
         #Testing our function
         x = turtlebot()
-        x.final()
+        x.move_turtle()
         
 
     except rospy.ROSInterruptException: pass
+
